e2400da7efdc4c283fa2c54489325c148c702795
bernigandara <2021283@student.cct.ie>
2023-04-09 10:23:42 +0100

    GameFrame and GamePanel extensions and implementations added

---
+++ src/snakegame/GameFrame.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakegame;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author bernardogandara
+ */
+public class GameFrame extends JFrame{
+    
+}
---
+++ src/snakegame/GamePanel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakegame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author bernardogandara
+ */
+public class GamePanel extends JPanel implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
