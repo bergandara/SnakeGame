4d90c94c42cf08b8806394fb459d29a8a44679f6
bernigandara <2021283@student.cct.ie>
2023-04-09 10:37:27 +0100

    Methods created in GamePanel

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -5,10 +5,11 @@
  */
 package snakegame;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 import javax.swing.JPanel;
-
 /**
  *
  * @author bernardogandara
@@ -18,9 +19,43 @@ public class GamePanel extends JPanel implements ActionListener{
         
     }
     
+    public void startGame(){
+        
+    }
+    
+    public void paintComponent(Graphics g){
+        
+    }
+    
+    public void draw(Graphics g){
+        
+    }
+    
+    public void move(){
+        
+    }
+    
+    public void checkApple(){
+        
+    }
+    
+    public void checkCollisions(){
+        
+    }
+    
+    public void gameOver(Graphics g){
+        
+    }
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public class myKeyAdapter extends KeyAdapter{
+        public void KeyPressed(KeyEvent e){
+            
+        }
     }
     
 }
