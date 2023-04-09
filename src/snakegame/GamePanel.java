beca9bf1417d12d9ca43fd528078a0156905eaa2
bernigandara <2021283@student.cct.ie>
2023-04-09 10:43:30 +0100

    All we need to declare before the constructor in GamePanel

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -15,6 +15,23 @@ import javax.swing.JPanel;
  * @author bernardogandara
  */
 public class GamePanel extends JPanel implements ActionListener{
+    
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75; //higher number = slower game
+    final int x[] = new int[GAME_UNITS];    //will hold x coordinates of the body parts of the snake
+    final int y[] = new int[GAME_UNITS];    //will hold y coordinates of the body parts
+    int bodyParts = 6;                      //Initial bodyparts of the snake
+    int applesEaten;
+    int appleX;                         //coordinate of where the apple is located
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+    
     GamePanel(){    //GamePanel constructor
         
     }
@@ -53,6 +70,7 @@ public class GamePanel extends JPanel implements ActionListener{
     }
     
     public class myKeyAdapter extends KeyAdapter{
+        @Override
         public void KeyPressed(KeyEvent e){
             
         }
