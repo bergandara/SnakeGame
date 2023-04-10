0b6aef53fbede9c02ebe6c212481a50b68a795ec
bernigandara <2021283@student.cct.ie>
2023-04-10 22:03:51 +0100

    draw commented and snakebody draw added

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -55,12 +55,27 @@ public class GamePanel extends JPanel implements ActionListener{
     }
     
     public void draw(Graphics g){
+        //COORDINATES DRAW
         for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
             g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
             g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
         }
+        
+        //DRAWING THE APPLE
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        
+        
+        //DRAWING THE SNAKE BODY
+        for(int i = 0; i < bodyParts; i++){
+            if(i == 0){
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }else {
+                g.setColor(new Color(45,180,0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
     }
     
     public void newApple(){
