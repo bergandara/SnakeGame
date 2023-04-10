8a939213232823220d5b375936586df1a1734ed4
bernigandara <2021283@student.cct.ie>
2023-04-10 22:14:45 +0100

    checkCollisions() method updated. head collision with body check head touches left and right border check head touches top and bottom border check stop timer

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -110,6 +110,32 @@ public class GamePanel extends JPanel implements ActionListener{
     
     public void checkCollisions(){
         
+        //Checks if heads collides with body
+        for(int i = bodyParts; i>0; i--){
+            if((x[0] == x[i]) && (y[0] == y[i])){   //This means the head as collided with the body
+                running = false;
+            }
+        }
+        //Check if head touches left border
+        if(x[0] < 0){
+            running = false;
+        }
+        //Check if head touches right border
+        if(x[0] > SCREEN_WIDTH){
+            running = false;
+        }
+        //Check if head touches top border
+        if(y[0] < 0){
+            running = false;
+        }
+        //Check if head touches bottom border
+        if(y[0] > SCREEN_HEIGHT){
+            running = false;
+        }
+        
+        if(!running){
+            timer.stop();
+        }
     }
     
     public void gameOver(Graphics g){
