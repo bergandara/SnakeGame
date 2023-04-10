bb57818bbf736fa16a58cadfd5a8d939851b9fc2
bernigandara <2021283@student.cct.ie>
2023-04-10 22:54:09 +0100

    gameOver() Method Display score

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -161,7 +161,7 @@ public class GamePanel extends JPanel implements ActionListener{
         g.setColor(Color.red);
         g.setFont(new Font("Ink Free", Font.BOLD, 40));
         FontMetrics metrics1 = getFontMetrics(g.getFont());
-        g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + applesEaten))/2, g.getFont().getSize());
+        g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: " + applesEaten))/2, g.getFont().getSize());
 
         //Game Over text
         g.setColor(Color.red);
